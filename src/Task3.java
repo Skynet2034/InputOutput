47f5962ae1dccbbe31877e61786dd0868a85783a
Иван <ivan1978_04@mail.ru>
2019-11-28 23:14:05 +0300

    With data file for task 3 >10MB + loading time tracking

--- src/Task3.java
+++ src/Task3.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.Arrays;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,29 +16,35 @@ public class Task3 {
 private static void readPage(RandomAccessFile file, long pageNumber, long pagesTotal) throws IOException {
     if ((pageNumber < 0) || (pageNumber > pagesTotal))
     { System.out.println("No such page"); return;}
+    long start=System.currentTimeMillis();
     long readLegth=PAGE_SIZE;
-    if ((pageNumber+1)*PAGE_SIZE>file.length()) readLegth=(pageNumber+1)*PAGE_SIZE-file.length();
+    if ((pageNumber+1)*PAGE_SIZE>file.length()) readLegth=file.length()-pageNumber*PAGE_SIZE;//если до конца файла меньше 1 полной страницы
     file.seek(pageNumber*PAGE_SIZE);
     byte[] rawPage=new byte[(int)readLegth];
     file.readFully(rawPage);
     String page=new String(rawPage, "CP1251");
     System.out.println(page);
+    System.out.printf("Page loaded in "+(System.currentTimeMillis()-start)+" milliseconds");
+    System.out.println();
    // file.seek(pageNumber*PAGE_SIZE+readLegth);
 }
-private static void printInfo(File fileName, long position, long length)
+private static void printInfo(File fileName, long pageCurrent, long pagesTotal)
 {
-    if (position==-1) position=0;
-    System.out.println("File name - "+fileName.getName()+" page "+(position+1)+" of "+(length+1));
+    if (pageCurrent==-1) pageCurrent=0;
+    System.out.println("File name - "+fileName.getName()+" page "+(pageCurrent+1)+" of "+(pagesTotal+1));
     System.out.println("Press N for next page, P for previous page, X for exit or enter page number");
 }
     public static void main(String[] args) {
 
         File fileName = new File("./data/task3/test1.txt");
+        long start=System.currentTimeMillis();
 exit:
         try (RandomAccessFile file=new RandomAccessFile(fileName,"r"); Scanner in=new Scanner(System.in)) {
             long length = file.length();
-            long pagesTotal = length / PAGE_SIZE-1;
+            long pagesTotal = length / PAGE_SIZE;
             long pageCurrent = -1;
+            System.out.printf("File loaded in "+(System.currentTimeMillis()-start)+" milliseconds");
+            System.out.println();
             while (true) {
                 printInfo(fileName, pageCurrent, pagesTotal);
                 long position = file.getFilePointer();
@@ -47,8 +52,8 @@ exit:
                 String page;
                 String input = in.nextLine().trim().toLowerCase();
                 if (input.matches("[0-9]+")) {
-                    int pageNumber = Integer.valueOf(input);
-                    readPage(file, pageNumber-1, pagesTotal);
+                    int pageNumber = Integer.valueOf(input)-1;
+                    readPage(file, pageNumber, pagesTotal);
                     } else if ((input.matches("[npx]")) && (input.length() == 1)) {
                     char tmp = input.charAt(0);
                     switch (tmp) {
@@ -58,7 +63,7 @@ exit:
                              }
                 }
                 else System.out.println("Bad command");
-                 pageCurrent = file.getFilePointer() / PAGE_SIZE-1;
+                 pageCurrent = file.getFilePointer() / PAGE_SIZE;
             }
 
         }
